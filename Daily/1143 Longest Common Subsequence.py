a88f764c067747317bdc67ea796b33ace33d8be2
EVASHINJI <rei.evangelion@qq.com>
2019-08-25 23:57:05 +0800

    2019/08/25

---
+++ Daily/1143 Longest Common Subsequence.py
@@ -0,0 +1,26 @@
+# Url: https://leetcode.com/problems/longest-common-subsequence/
+# Related Topics:
+# DP
+
+# Example 1:
+# Input: text1 = "abcde", text2 = "ace" 
+# Output: 3  
+# Explanation: The longest common subsequence is "ace" and its length is 3.
+
+# Example 2:
+# Input: text1 = "abc", text2 = "abc"
+# Output: 3
+# Explanation: The longest common subsequence is "abc" and its length is 3.
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        len1, len2 = len(text1), len(text2)
+        dp = [[0] * (len2 + 1) for _ in range(len1+1)]
+        for i in range(len1):
+            for j in range(len2):
+                if text1[i] == text2[j]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        return dp[len1-1][len2-1]
